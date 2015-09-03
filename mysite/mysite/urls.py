aa773ebf245ba586550e57a16fa1c7a0732cc3db
Jessica Larsen <jess_larsen@hotmail.com>
2015-09-02 18:41:26 -0600

    added landing page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-   url(r'^polls/', include('polls.urls', namespace="polls")),
+   url(r'^', include('polls.urls', namespace="polls")),
    url(r'^admin/', include(admin.site.urls)),
 ]
